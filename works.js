d3672f1f4e0c479a80125c3967b8c60fda8f28b6
Amin Musah Ahmed <ameenmusah77@gmail.com>
2020-08-18 08:07:50 +0800

    modified the pop up menu

--- works.js
+++ works.js
@@ -1,13 +1,9 @@
 const load = document.querySelector('#load');
-const select = document.querySelector('.select');
-const text = document.querySelector('h1')
+const wrapper = document.querySelector('.wrapper');
+const text = document.querySelector('h1');
 
 
 load.addEventListener('click', () => {
-    if (select.style.display === 'none'){
-      select.style.display = 'flex';  
-    } else {
-        select.style.display = 'none'
-    }
-    
+    wrapper.classList.toggle('active');
+    load.classList.toggle('toggle')
 })
